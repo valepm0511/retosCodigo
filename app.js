88d1ffff8cd235ea4f3f8be38dcd24a3186b0cac
valeria <valeria.penaloza5@gmail.com>
2018-06-05 21:19:03 -0400

    ejercicio 2 listo

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-//Escriba una función 'computeAverageLengthOfWords' 
-//que recibe dos strings y devuelve el promedio de
-//la longitud de ambos strings.
+/*Escriba una función 'computeAverageLengthOfWords' 
+que recibe dos strings y devuelve el promedio de
+la longitud de ambos strings.*/
 
 const computeAverageLengthOfWords = (word1, word2) => {
 	let sum = word1.length+word2.length;
 	let average = sum / 2;
 	return average;
 }
+
+/*Dando un nombre y una contraseña, areValidCredentials, devuelve true si el nombre es superior a 3 caracteres, Y, la contraseña tiene al menos 8 caracteres de longitud. De lo contrario, devuelve false.*/
+
+const areValidCredentials = (name, password) => {
+	if(name.length >= 3 && password.length >= 8){
+		return true;
+	}else{
+		return false;
+	}
+}
+
